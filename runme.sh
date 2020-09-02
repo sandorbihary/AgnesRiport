3f261a5769323562eb01913950c1c644c878fa74
sandorbihary <sandor.bihary@gmail.com>
2020-09-02 21:29:03 +0200

    new stuff

--- runme.sh
+++ runme.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
-#spark-submit covid.py
+wget --no-check-certificate https://covid.ourworldindata.org/data/owid-covid-data.csv
+hdfs dfs -put owid-covid-data.csv
+
+spark-submit covid.py
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+echo "sparsubmitdone" >> runs.txt
+
 python charting.py
-git add *.png
+
+git add .
 git commit -m "new stuff"
 git push
+
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
+echo $(date) >> runs.txt
