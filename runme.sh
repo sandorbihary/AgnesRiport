8bbf88ce3165260fca9393fec9e1aee40cd50aa3
sandorbihary <sandor.bihary@gmail.com>
2020-09-06 12:34:23 +0200

    new stuff

--- runme.sh
+++ runme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -x
 wget --backups=1 --no-check-certificate https://covid.ourworldindata.org/data/owid-covid-data.csv
 
-hdfs dfs -f -put owid-covid-data.csv
+hdfs dfs -rm owid-covid-data.csv
+hdfs dfs -put owid-covid-data.csv
 
 spark-submit covid.py
 echo "sparsubmitdone" >> runs.txt
